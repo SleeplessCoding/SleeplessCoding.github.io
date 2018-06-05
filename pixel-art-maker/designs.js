aa281dadc10223988ae514f265f70129b5f4d0d6
Nick Weinold <sleeplesscoding@gmail.com>
2018-06-05 04:37:08 -0700

    Added height and width listeners

--- pixel-art-maker/designs.js
+++ pixel-art-maker/designs.js
@@ -1,10 +1,25 @@
+// Declare variables
+
+var heightInput;
+var widthInput;
+
+// Add event listeners
+
+// height input listener
+
+function getHeight(event) {
+    heightInput = event.target.value;
+}
+
+function getWidth() {
+    widthInput = event.target.value;
+}
+
+// width input listener
+
+
 // Select color input
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-}
